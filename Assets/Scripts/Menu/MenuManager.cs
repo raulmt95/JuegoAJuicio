49b6456501385dc153fd946f2b49bb07e046047e
Javier <jasercam97@gmail.com>
2021-08-25 14:37:14 +0200

    Canvas placeholder

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -3,15 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject MainPanel;
+    public GameObject CreditsPanel;
+    public GameObject SettingsPanel;
+
+    [Header("Move Panels")]
+    public float MoveTime;
+    public Transform ShowPanelPos;
+    public Transform HidePanelPos;
+
+    private Vector3 hiddenPanelPos;
+    private GameObject shownPanel;
+    private GameObject hiddenPanel;
+
+    private void Start()
+    {
+        hiddenPanelPos = CreditsPanel.transform.position;
+    }
+
+    void MoveMenuPanels(GameObject panelToShow, GameObject panelToHide)
+    {
+        panelToShow.transform.DOMoveY(ShowPanelPos.position.y, MoveTime).SetEase(Ease.OutQuad);
+        panelToHide.transform.DOMoveY(HidePanelPos.position.y, MoveTime).SetEase(Ease.OutQuad);
+        shownPanel = panelToShow;
+        hiddenPanel = panelToHide;
+        Invoke(nameof(SetPanelsPosition), MoveTime + 0.5f);
+    }
+
+    private void SetPanelsPosition()
+    {
+        hiddenPanel.transform.position = hiddenPanelPos;
+    }
 
     #region BUTTONS
 
     public void OnPlayButtonPressed(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void OnCreditsButtonPressed()
+    {
+        MoveMenuPanels(CreditsPanel, MainPanel);
+    }
+    public void OnSettingsButtonPressed()
+    {
+        MoveMenuPanels(SettingsPanel, MainPanel);
+    }
+    public void OnBackButtonPressed()
+    {
+        MoveMenuPanels(MainPanel, shownPanel);
     }
 
     public void OnExitButtonPressed()
