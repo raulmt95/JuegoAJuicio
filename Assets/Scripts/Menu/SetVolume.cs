17830ec49dfa227a06b38df40cb6149ae4d63e1c
Javier <jasercam97@gmail.com>
2021-08-25 23:59:33 +0200

    Me estoy cabreando

---
+++ Assets/Scripts/Menu/SetVolume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer Mixer;
+
+    public void SetVolumeLevel(float value, bool isMusic)
+    {
+        if (isMusic) { Mixer.SetFloat("MusicVol", Mathf.Log10(value) * 20); }
+        else { Mixer.SetFloat("SFXVol", Mathf.Log10(value) * 20); }
+    }
+}
---
+++ Assets/Scripts/Menu/SoundHandler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SoundHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    public float DragLimit;
+    public GameObject SoundMarker;
+    public bool IsMusic;
+    
+    public event Action<Quaternion> OnAngleChanged;
+ 
+    Quaternion dragStartRotation;
+    Quaternion dragStartInverseRotation;
+
+    private SetVolume volumeSetter;
+ 
+    private void Awake()
+    {
+        OnAngleChanged += (rotation) => transform.localRotation = rotation;
+    }
+
+    void Start()
+    {
+        volumeSetter = GetComponent<SetVolume>();
+        volumeSetter.SetVolumeLevel(CalculateRotationLevel(transform.localRotation.eulerAngles.z), IsMusic);
+    }
+
+    void Update()
+    {
+        if(transform.localRotation.eulerAngles.z > DragLimit && transform.localRotation.eulerAngles.z < 360 - DragLimit)
+        {
+            float aux = transform.localRotation.eulerAngles.z;
+            aux = 360 - aux;
+            if(aux > 360 / 2) { transform.localRotation = Quaternion.Euler(new Vector3(0, 0, DragLimit)); }
+            else { transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -DragLimit)); }
+        }
+
+        SoundMarker.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, transform.localRotation.eulerAngles.z * -1));
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragStartRotation = transform.localRotation;
+        Vector3 worldPoint;
+        if (DragWorldPoint(eventData, out worldPoint))
+        {
+            dragStartInverseRotation = Quaternion.Inverse(Quaternion.LookRotation(worldPoint - transform.position, Vector3.forward));
+        }
+        else
+        {
+            Debug.LogWarning("Couldn't get drag start world point");
+        }
+    }
+ 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Setear volumen
+        volumeSetter.SetVolumeLevel(CalculateRotationLevel(transform.localRotation.eulerAngles.z), IsMusic);
+    }
+ 
+    float CalculateRotationLevel(float zRotation)
+    {
+        zRotation += DragLimit;
+        if(zRotation == 360f) { zRotation = 0.0001f; }
+        else if(zRotation > DragLimit * 2)
+        {
+            zRotation -= 360f;
+        }
+
+        return zRotation/ (DragLimit * 2);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 worldPoint;
+        if (DragWorldPoint(eventData, out worldPoint))
+        {
+            Quaternion currentDragAngle = Quaternion.LookRotation(worldPoint - transform.position, Vector3.forward);
+            if (OnAngleChanged != null)
+            {
+                OnAngleChanged(currentDragAngle * dragStartInverseRotation * dragStartRotation);
+            }
+        }
+    }
+ 
+    private bool DragWorldPoint(PointerEventData eventData, out Vector3 worldPoint)
+    {
+        return RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            GetComponent<RectTransform>(),
+            eventData.position,
+            eventData.pressEventCamera,
+            out worldPoint);
+    }
+}
