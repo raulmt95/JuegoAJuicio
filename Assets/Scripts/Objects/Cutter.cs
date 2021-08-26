18d35384edf92e192229241cf071a7a19dc48549
Lindryn <alomis1995@gmail.com>
2021-08-26 15:53:23 +0200

    Peluquero

--- Assets/Scripts/Objects/Cutter.cs
+++ Assets/Scripts/Objects/Cutter.cs
@@ -8,13 +8,20 @@ public class Cutter: MonoBehaviour
     public Rigidbody2D Player_rb;
     public float Cooldown = 1f;
 
+    private Animator _anim;
+
     private bool _able = true;
 
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Hair") && _able)
         {
             //_joint.distance = Vector2.Distance(transform.position, Player_rb.transform.position);
+            _anim.SetTrigger("Cut");
             collision.GetComponent<Hair>().CutHair(Vector2.Distance(transform.position, Player_rb.transform.position));
             _able = false;
             Invoke(nameof(Restore), Cooldown);
