e77d0c735bc92656ab12dbcc030382f4d60e391b
Javier <jasercam97@gmail.com>
2021-08-27 11:56:30 +0200

    Sistema de pausa en escena duplicada

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public KeyCode PauseKey;
+
+    public GameObject PausePanel;
+    public GameObject ExitConfirmationPanel;
+
+    public float PopupAnimationDuration;
+
+    public int MenuSceneIndex;
+
+    private bool _isGamePaused;
+
+    private void Start()
+    {
+        PausePanel.transform.localScale = Vector3.zero;
+        ExitConfirmationPanel.transform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(PauseKey) && !_isGamePaused) { OnPauseButtonPressed(); }
+    }
+
+    void SetPanelScale(GameObject panel, Vector3 scale)
+    {
+        panel.transform.DOScale(scale, PopupAnimationDuration).SetUpdate(true);
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="timeScale">0 to pause, 1 to resume</param>
+    public void SetGameTimeScale(float timeScale)
+    {
+        Time.timeScale = timeScale;
+    }
+
+    void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    #region BUTTONS
+
+    public void OnPauseButtonPressed()
+    {
+        SetGameTimeScale(0);
+        SetPanelScale(PausePanel, Vector3.one);
+        _isGamePaused = true;
+    }
+
+    public void OnResumeGameButtonPressed()
+    {
+        SetPanelScale(PausePanel, Vector3.zero);
+        SetGameTimeScale(1);
+        _isGamePaused = false;
+    }
+
+    public void OnExitButtonPressed()
+    {
+        SetPanelScale(ExitConfirmationPanel, Vector3.one);
+    }
+
+    public void OnExitConfirmed()
+    {
+        LoadSceneByIndex(MenuSceneIndex);
+    }
+
+    public void OnExitRejected()
+    {
+        SetPanelScale(ExitConfirmationPanel, Vector3.zero);
+    }
+
+    #endregion
+}
