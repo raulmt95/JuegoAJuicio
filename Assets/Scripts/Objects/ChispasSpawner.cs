e5948f94e347e0efbd3f6a4b3b928ae91dd7e374
Lindryn <alomis1995@gmail.com>
2021-08-29 17:28:17 +0200

    Engranajes con chispas

---
+++ Assets/Scripts/Objects/ChispasSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChispasSpawner : MonoBehaviour
+{
+    public GameObject ChispitasPS;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Chispas), 0f, Random.Range(5f, 15f));
+    }
+    private void Chispas()
+    {
+        if (ChispitasPS)
+        {
+            if (Random.value > 0.9)
+                Instantiate(ChispitasPS, transform.position, ChispitasPS.transform.rotation);
+        }
+    }
+}
--- Assets/Scripts/Objects/Trap.cs
+++ Assets/Scripts/Objects/Trap.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    public GameObject ChispitasPS;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Chispas), 0f, Random.Range(5f, 15f));
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Hair"))
         {
             collision.GetComponent<Hair>().TrapHair(true);
+        }
+    }
+
+
+    private void Chispas()
+    {
+        if (ChispitasPS)
+        {
+            if (Random.value > 0.9)
+                Instantiate(ChispitasPS, transform.position, ChispitasPS.transform.rotation);
         }
     }
 }
