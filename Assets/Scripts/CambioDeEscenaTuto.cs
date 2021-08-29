4b6323623f6680ec47c103fdcf26da50887d8d08
Lindryn <alomis1995@gmail.com>
2021-08-29 19:49:35 +0200

    Transicion entre escenas

---
+++ Assets/Scripts/CambioDeEscenaTuto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscenaTuto : MonoBehaviour
+{
+    public Animator Anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Anim.SetTrigger("Out");
+            Invoke(nameof(ChangeScene), 0.5f);
+        }
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
