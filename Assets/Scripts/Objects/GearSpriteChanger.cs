c6464a56aca2ddcff14041239261cd8ca1d46b7e
Raul Moreno Tamayo <raulmorenotamayo@gmail.com>
2021-08-30 17:13:21 +0200

    engranajes distintos

---
+++ Assets/Scripts/Objects/GearSpriteChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearSpriteChanger : MonoBehaviour
+{
+    public SpriteRenderer MainRenderer;
+    public SpriteRenderer MaskRenderer;
+    
+    public List<Sprite> SpriteList;
+
+    private void Start()
+    {
+        int randomNumber = Random.Range(0, SpriteList.Count);
+
+        MainRenderer.sprite = SpriteList[randomNumber];
+        MaskRenderer.sprite = SpriteList[randomNumber];
+    }
+}
