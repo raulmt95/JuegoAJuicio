e39e965dd58ad06191afafcaa23026a34447f43b
Javier <jasercam97@gmail.com>
2021-08-30 18:24:20 +0200

    CanvasIngame

---
+++ Assets/Scripts/Menu/SoundSliders.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SoundSliders : MonoBehaviour
+{
+    public TMP_Text VolumeText;
+
+    private Slider _slider;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    public void SetVolumePercentage()
+    {
+        int volume = (int)((_slider.value / _slider.maxValue) * 100);
+        VolumeText.text = volume.ToString() + "%";
+    }
+}
