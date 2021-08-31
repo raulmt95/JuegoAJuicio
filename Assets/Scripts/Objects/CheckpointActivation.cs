ac739949395d44a6687535dc26ed4db9e7a11365
Javier <jasercam97@gmail.com>
2021-08-31 20:11:21 +0200

    Prefab Checkpoint

---
+++ Assets/Scripts/Objects/CheckpointActivation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointActivation : MonoBehaviour
+{
+    public List<GearRotation> GearsList;
+    public GameObject YellowLight; //Poner particula dentro de la bombilla y cambiar la referencia. Dejar la particula desactivada.
+    public GameObject RedLight; //Poner particula dentro de la bombilla y cambiar la referencia. Dejar la particula activada.
+
+
+    //Cuando el player toca el checkpoint acceder a este script y llamar SetCheckpointState(true). Se puede llamar antes (false) al checkpoint anterior si se quiere desactivar.
+    public void SetCheckpointState(bool state)
+    {
+        SetGearRotationState(state);
+
+        YellowLight.SetActive(state);
+        RedLight.SetActive(!state);
+    }
+
+    void SetGearRotationState(bool state)
+    {
+        foreach(GearRotation gear in GearsList)
+        {
+            gear.SetRotation(state);
+        }
+    }
+
+}
