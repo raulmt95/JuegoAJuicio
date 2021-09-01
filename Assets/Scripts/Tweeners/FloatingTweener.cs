a047881f32bc32ca3c14d98957c280c4219d5b19
Lindryn <alomis1995@gmail.com>
2021-09-01 14:34:03 +0200

    Cambios bonicos

---
+++ Assets/Scripts/Tweeners/FloatingTweener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FloatingTweener : MonoBehaviour
+{
+    public float MovimientoMax = 5f;
+
+    void Start()
+    {
+        transform.DOLocalMoveY(transform.position.y - MovimientoMax, 2f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine).Play();
+    }
+}
